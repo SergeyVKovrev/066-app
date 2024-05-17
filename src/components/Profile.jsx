cd04e462d3451987373a16ebcc6eeb0d9d3ce952
Sergey V.Kovrev <podoxla@list.ru>
2024-05-17 15:21:58 +0300

    Fix md

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,28 @@
 export const Profile = () => {
-  return <h1>Профиль</h1>;
+  return (
+    <div className="row">
+      <div className="col-md-4">
+        <img
+          src="https://img.freepik.com/free-psd/3d-illustration-bald-person-with-glasses_23-2149436184.jpg"
+          alt=""
+          width="100%"
+        />
+      </div>
+      <div className="col-md-8">
+        <p>
+          <span>Id: </span>
+          <span>2</span>
+        </p>
+        <p>
+          <span>Email:</span>
+          <span>vania@mail.ru</span>
+        </p>
+        <p>
+          <span>Фамилия имя: </span>
+          <span>Иван Иванов</span>
+        </p>
+        <p>Рассказ о себе</p>
+      </div>
+    </div>
+  );
 };
