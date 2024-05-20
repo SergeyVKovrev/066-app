d3360d1113aa04d8e753150b36cf56711bd7c8e5
Sergey V.Kovrev <podoxla@list.ru>
2024-05-20 17:33:21 +0300

    занятие 20 числа

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const TableRow = (props) => {
   return (
     <tr>
       <th scope="row">{props.index + 1}</th>
       <td>
-        {props.name} {props.lastname}
+        <NavLink to={"/profile/" + props.id}>
+          {props.name} {props.lastname}
+        </NavLink>
       </td>
+      <td>{props.email}</td>
+      <td>{props.id}</td>
     </tr>
   );
 };
@@ -15,7 +21,16 @@ export const Settings = (props) => {
   let usersCount = Object.keys(users).length;
   let userRow = [];
   for (let i = 0; i < usersCount; i++) {
-    userRow.push(<TableRow name={users[i].name} lastname={users[i].lastname} index={i} key={i} />);
+    userRow.push(
+      <TableRow
+        name={users[i].name}
+        lastname={users[i].lastname}
+        index={i}
+        key={i}
+        email={users[i].email}
+        id={users[i].id}
+      />
+    );
   }
   return (
     <div className="row">
@@ -24,6 +39,8 @@ export const Settings = (props) => {
           <tr>
             <th scope="col">#</th>
             <th scope="col">Имя и фамилия</th>
+            <th scope="col">Email</th>
+            <th scope="col">Id</th>
           </tr>
         </thead>
         <tbody>{userRow}</tbody>
